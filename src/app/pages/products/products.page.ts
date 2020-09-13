e454a171539ab8e7e7c20230eb04512505e0b5c4
Ravi Angara <ravi.angara@prolifics.com>
2020-09-13 10:17:10 +0530

    Prod details

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -8,11 +8,23 @@ import { MenuController } from '@ionic/angular';
 })
 export class ProductsPage implements OnInit {
 
-  constructor(private menu: MenuController) {    
+  isProdList: boolean = true;
+  isProdDet: boolean = false;
+
+  constructor(private menu: MenuController) {
     this.menu.close();
   }
 
   ngOnInit() {
+  }
+
+  showProdList() {
+    this.isProdList = true;
+    this.isProdDet = false;
+  }
+  showProdDet() {
+    this.isProdList = false;
+    this.isProdDet = true;
   }
 
 }
